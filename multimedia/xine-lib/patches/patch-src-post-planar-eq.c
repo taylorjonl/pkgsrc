e924c2ebbf7f4e8e2051f2e574688efc98cccdf2
Jonathan Perkin <jperkin@joyent.com>
2014-01-21 15:18:47 +0000

    Revert "Resolve MMX register allocation."
    This reverts commit c35d07f7ae831aa51f1d4986e8a21cd2454219c1.

--- multimedia/xine-lib/patches/patch-src-post-planar-eq.c
+++ multimedia/xine-lib/patches/patch-src-post-planar-eq.c
@@ -1,34 +1,8 @@
 $NetBSD: patch-src-post-planar-eq.c,v 1.1 2011/09/04 14:34:52 joerg Exp $
 
-Re-arrange inline asm to resolve 'error: can't find a register' failures.
-
---- src/post/planar/eq.c.orig	2013-09-18 10:04:54.000000000 +0000
+--- src/post/planar/eq.c.orig	2011-08-30 13:13:46.000000000 +0000
 +++ src/post/planar/eq.c
-@@ -50,8 +50,12 @@ static void process_MMX(unsigned char *d
- 
- 	while (h--) {
- 		asm volatile (
--			"movq (%5), %%mm3 \n\t"
--			"movq (%6), %%mm4 \n\t"
-+			"movq (%0), %%mm3 \n\t"
-+			"movq (%1), %%mm4 \n\t"
-+			:
-+			: "r" (brvec), "r" (contvec)
-+		);
-+		asm volatile (
- 			"pxor %%mm0, %%mm0 \n\t"
- 			"movl %4, %%eax\n\t"
- 			ASMALIGN(4)
-@@ -73,7 +77,7 @@ static void process_MMX(unsigned char *d
- 			"decl %%eax \n\t"
- 			"jnz 1b \n\t"
- 			: "=r" (src), "=r" (dest)
--			: "0" (src), "1" (dest), "r" (w>>3), "r" (brvec), "r" (contvec)
-+			: "0" (src), "1" (dest), "r" (w>>3)
- 			: "%eax"
- 		);
- 
-@@ -138,9 +142,11 @@ typedef struct eq_parameters_s {
+@@ -138,9 +138,11 @@ typedef struct eq_parameters_s {
   * description of params struct
   */
  START_PARAM_DESCR( eq_parameters_t )
