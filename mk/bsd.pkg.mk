778176d0d27a10ecb98b8970a726b412d8890ada
tnn <tnn>
2015-03-07 14:17:51 +0000

    Adopt USE_GITHUB from FreeBSD ports to make github MASTER_SITE handling less painful. See: https://www.freebsd.org/doc/en_US.ISO8859-1/books/porters-handbook/makefile-distfiles.html#makefile-master_sites-github
    To use, set in Makefile:
    
    DISTNAME=	exampleproject-1.2
    USE_GITHUB=	YES
    
    The following variables alter USE_GITHUB behavior:
    
    GH_ACCOUNT	defaults to PKGBASE
    GH_PROJECT	defaults to PKGBASE
    GH_TAGNAME	defaults to PKGVERSION_NOREV
    		(sometimes you want to override with v${PKGVERSION_NOREV})
    GH_COMMIT	explicit commit hash if no tag is available
    GH_RELEASE	default empty, may be set to ${DISTNAME} for example
    GH_TYPE		overrides the autodetected MASTER_SITE URL scheme

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -96,7 +96,15 @@ MAINTAINER?=		pkgsrc-users@NetBSD.org
 .endif
 PKGWILDCARD?=		${PKGBASE}-[0-9]*
 TOOL_DEPENDS?=		# empty
+.if defined(USE_GITHUB) && !empty(USE_GITHUB:M[yY][eE][sS]) && \
+	defined(GH_COMMIT) && !empty(GH_COMMIT)
+WRKSRC?=		${WRKDIR}/${GH_PROJECT}-${GH_COMMIT}
+.elif defined(USE_GITHUB) && !empty(USE_GITHUB:M[yY][eE][sS]) && \
+	defined(GH_TAGNAME) && !empty(GH_TAGNAME)
+WRKSRC?=	${WRKDIR}/${GH_PROJECT}-${GH_TAGNAME:C/^v//}
+.else
 WRKSRC?=		${WRKDIR}/${DISTNAME:U${PKGNAME_NOREV}}
+.endif
 
 # Override for SU_CMD user check
 _IS_ROOT_CMD?=		${TEST}	`${ID} -u` = `${ID} -u ${_SU_ROOT_USER}`
