063bced8107c5369f8f6e8c3e9bbc2109ec70d4b
tron <tron>
2015-04-26 08:26:15 +0000

    Pullup ticket #4684 - requested by taca lang/ruby200-base: security update
    Revisions pulled up:
    - lang/ruby/rubyversion.mk                                      1.139
    - lang/ruby200-base/distinfo                                    1.23 via patch
    
    ---
       Module Name:	pkgsrc
       Committed By:	taca
       Date:		Sun Apr 19 16:12:23 UTC 2015
    
       Modified Files:
       	pkgsrc/lang/ruby: rubyversion.mk
       	pkgsrc/lang/ruby200-base: distinfo
    
       Log Message:
       Update ruby200 package to 2.0.0p645 (Ruby 2.0.0-p645).
    
       >From release announce:
    
       We are pleased to announce the release of Ruby 2.0.0-p645.
    
       This release includes a security fix for OpenSSL extension. Please view the
       topic below for more details.
    
           CVE-2015-1855: Ruby OpenSSL Hostname Verification
    
       Ruby 2.0.0 is now under the state of the security maintenance phase, until
       Feb. 24th, 2016. After the date, maintenance of Ruby 2.0.0 will be ended. We
       recommend you start planning migration to newer versions of Ruby, such as 2.1
       or 2.2.
    
       This release includes the security fix mentioned above along with small
       changes required for test environment (that shouldn't affect normal users).
    
       See ChangeLog for full details.

--- lang/ruby/rubyversion.mk
+++ lang/ruby/rubyversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: rubyversion.mk,v 1.136 2015/03/08 14:59:12 taca Exp $
+# $NetBSD: rubyversion.mk,v 1.138.2.1 2015/04/26 08:26:15 tron Exp $
 #
 
 # This file determines which Ruby version is used as a dependency for
@@ -228,7 +228,7 @@ RUBY21_VERSION=		2.1.5
 # patch
 RUBY18_PATCHLEVEL=	pl374
 RUBY193_PATCHLEVEL=	p551
-RUBY200_PATCHLEVEL=	p643
+RUBY200_PATCHLEVEL=	p645
 #RUBY21_PATCHLEVEL=	p273
 
 # current API compatible version; used for version of shared library
