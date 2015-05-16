150945df14df87a83a8537b670eb1e807bd7a86d
taca <taca>
2015-05-16 11:18:57 +0000

    Update php56 to 5.6.9.
    14 May 2015, PHP 5.6.9
    
    - Core:
      . Fixed bug #69467 (Wrong checked for the interface by using Trait).
        (Laruence)
      . Fixed bug #69420 (Invalid read in zend_std_get_method). (Laruence)
      . Fixed bug #60022 ("use statement [...] has no effect" depends on leading
        backslash). (Nikita)
      . Fixed bug #67314 (Segmentation fault in gc_remove_zval_from_buffer).
        (Dmitry)
      . Fixed bug #68652 (segmentation fault in destructor). (Dmitry)
      . Fixed bug #69419 (Returning compatible sub generator produces a warning).
        (Nikita)
      . Fixed bug #69472 (php_sys_readlink ignores misc errors from
           GetFinalPathNameByHandleA). (Jan Starke)
      . Fixed bug #69364 (PHP Multipart/form-data remote dos Vulnerability). (Stas)
      . Fixed bug #69403 (str_repeat() sign mismatch based memory corruption).
        (Stas)
      . Fixed bug #69418 (CVE-2006-7243 fix regressions in 5.4+). (Stas)
      . Fixed bug #69522 (heap buffer overflow in unpack()). (Stas)
    
    - FTP:
      . Fixed bug #69545 (Integer overflow in ftp_genlist() resulting in heap
        overflow). (Stas)
    
    - ODBC:
      . Fixed bug #69354 (Incorrect use of SQLColAttributes with ODBC 3.0).
        (Anatol)
      . Fixed bug #69474 (ODBC: Query with same field name from two tables returns
        incorrect result). (Anatol)
      . Fixed bug #69381 (out of memory with sage odbc driver). (Frederic Marchall,
          Anatol Belski)
    
    - OpenSSL:
      . Fixed bug #69402 (Reading empty SSL stream hangs until timeout).
        (Daniel Lowrey)
    
    - PCNTL:
      . Fixed bug #68598 (pcntl_exec() should not allow null char). (Stas)
    
    - PCRE
      . Upgraded pcrelib to 8.37.
    
    - Phar:
      . Fixed bug #69453 (Memory Corruption in phar_parse_tarfile when entry
        filename starts with null). (Stas)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.98 2015/05/16 11:17:45 taca Exp $
+# $NetBSD: phpversion.mk,v 1.99 2015/05/16 11:18:57 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -84,7 +84,7 @@ PHPVERSION_MK=	defined
 PHP53_VERSION=	5.3.29
 PHP54_VERSION=	5.4.41
 PHP55_VERSION=	5.5.25
-PHP56_VERSION=	5.6.8
+PHP56_VERSION=	5.6.9
 
 # Define initial release of major version.
 PHP53_RELDATE=	20090630
