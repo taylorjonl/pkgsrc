1b60ade941aa316be9a83698368b96545a2f9b6f
taca <taca>
2015-06-12 00:47:03 +0000

    Update php56 to 5.6.10.
    11 Jun 2015, PHP 5.6.10
    
    - Core:
      . Fixed bug #66048 (temp. directory is cached during multiple requests).
        (Julien)
      . Fixed bug #69566 (Conditional jump or move depends on uninitialised value
        in extension trait). (jbboehr at gmail dot com)
      . Fixed bug #69599 (Strange generator+exception+variadic crash). (Nikita)
      . Fixed bug #69628 (complex GLOB_BRACE fails on Windows).
        (Christoph M. Becker)
      . Fixed POST data processing slowdown due to small input buffer size
        on Windows. (Jorge Oliveira, Anatol)
      . Fixed bug #69646 (OS command injection vulnerability in escapeshellarg).
        (Anatol Belski)
      . Fixed bug #69719 (Incorrect handling of paths with NULs). (Stas)
    
    - FTP
      . Improved fix for bug #69545 (Integer overflow in ftp_genlist()
        resulting in heap overflow). (Max Spelsberg)
    
    - GD:
      . Fixed bug #69479 (GD fails to build with newer libvpx). (Remi)
    
    - Iconv:
      . Fixed bug #48147 (iconv with //IGNORE cuts the string). (Stas)
    
    - Litespeed SAPI:
      . Fixed bug #68812 (Unchecked return value). (George Wang)
    
    - Mail:
      . Fixed bug #68776 (mail() does not have mail header injection prevention for
        additional headers). (Yasuo)
    
    - MCrypt:
      . Added file descriptor caching to mcrypt_create_iv() (Leigh)
    
    - Opcache
      . Fixed bug #69549 (Memory leak with opcache.optimization_level=0xFFFFFFFF).
        (Laruence, Dmitry)
    
    - Phar:
      . Fixed bug #69680 (phar symlink in binary directory broken).
        (Matteo Bernardini, Remi)
    
    - Postgres:
      . Fixed bug #69667 (segfault in php_pgsql_meta_data). (Remi)
    
    - Sqlite3:
      . Upgrade bundled sqlite to 3.8.10.2. (CVE-2015-3414, CVE-2015-3415,
        CVE-2015-3416) (Kaplan)

--- lang/php/phpversion.mk
+++ lang/php/phpversion.mk
@@ -1,4 +1,4 @@
-# $NetBSD: phpversion.mk,v 1.100 2015/06/12 00:44:32 taca Exp $
+# $NetBSD: phpversion.mk,v 1.101 2015/06/12 00:47:03 taca Exp $
 #
 # This file selects a PHP version, based on the user's preferences and
 # the installed packages. It does not add a dependency on the PHP
@@ -84,7 +84,7 @@ PHPVERSION_MK=	defined
 PHP53_VERSION=	5.3.29
 PHP54_VERSION=	5.4.41
 PHP55_VERSION=	5.5.26
-PHP56_VERSION=	5.6.9
+PHP56_VERSION=	5.6.10
 
 # Define initial release of major version.
 PHP53_RELDATE=	20090630
