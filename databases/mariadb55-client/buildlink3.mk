02457baf28fb31cb36007e4d2e9a5d08e645863a
Filip Hajny <filip@joyent.com>
2015-08-11 10:53:11 +0000

    Limit mariadb depends properly for 5.5

--- databases/mariadb55-client/buildlink3.mk
+++ databases/mariadb55-client/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	mariadb-client
 .if !defined(MARIADB_CLIENT_BUILDLINK3_MK)
 MARIADB_CLIENT_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mariadb-client+=	mariadb-client>=5.5.43
+BUILDLINK_API_DEPENDS.mariadb-client+=	mariadb-client>=5.5.43<10.0
 BUILDLINK_PKGSRCDIR.mariadb-client?=	../../databases/mariadb55-client
 BUILDLINK_PKGSRCDIR.mariadb-client?=	../../databases/mariadb55-client
 BUILDLINK_INCDIRS.mariadb-client?=	include/mysql
