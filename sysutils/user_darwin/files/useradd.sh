8a82c0c16df53e4f3ccbfa682780ee3eb8a7f5d9
Jonathan Perkin <jperkin@joyent.com>
2015-09-02 14:43:44 +0100

    Stop users from appearing in the OSX login window.

--- sysutils/user_darwin/files/useradd.sh
+++ sysutils/user_darwin/files/useradd.sh
@@ -97,6 +97,7 @@ else
     dscl . -create /users/$user UserShell "$shell"
     dscl . -create /users/$user Comment "$comment"
     dscl . -create /users/$user Password '*'
+    dscl . -delete /users/$user AuthenticationAuthority
 fi
 
 if ! nireport . /users/uid=$uid uid 2>/dev/null &&			\
